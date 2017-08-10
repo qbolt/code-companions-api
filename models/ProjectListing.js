3a19739b4dedda586f891b4d5ab28eea3328d938
Quinton Bolt <bolt.quinton@yahoo.com>
2017-08-10 18:00:21 -0500

    applied consistent styling and some light refactoring

--- models/ProjectListing.js
+++ models/ProjectListing.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://fujfntzw:KPGbWEPIl_qrZ8hHCjqqHnB8VT4hfsC8@babar.elephantsql.com:5432/fujfntzw',{define: {underscored: true}});
+const sequelize = new Sequelize('postgres://fujfntzw:KPGbWEPIl_qrZ8hHCjqqHnB8VT4hfsC8@babar.elephantsql.com:5432/fujfntzw', { define: { underscored: true } });
 
 module.exports = sequelize.define('project_listing', {
     project_creator: {
